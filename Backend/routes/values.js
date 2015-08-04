fe2ab58af1f734085d8cda295049686f77588cba
Robert Lübke <robert.luebke@tu-dresden.de>
2015-08-04 10:20:26 +0200

    timestamp correction

--- Backend/routes/values.js
+++ Backend/routes/values.js
@@ -1,12 +1,30 @@
 var pipelinr_util = require("../util/util.js");
-var models = require('../models/models.js'); 
+var models = require('../models/models.js');
+var moment = require('../node_modules/moment/min/moment.min.js');
 
 exports.updateValue = function(req, res) {
   var object = req.body;
   var _pipeline_id = req.params.pipeline_id;
   var _dataset_id = req.params.dataset_id;
-
-  //console.log('Update value: ' + _pipeline_id + " in " + _dataset_id);
+  //Correct the timestamp if neccessary
+  var ts;
+  if (object.timestamp === null || object.timestamp === undefined) {
+    //No timestamp given, just take the current time of the pipelinr machine
+    ts = moment().format('DD MM YYYY, HH:mm:ss:SSS');
+  } else if (moment(object.timestamp, 'DD MM YYYY, HH:mm:ss:SSS').isValid()) {
+    //Timestamp already has the expected database format, just use it
+    ts = object.timestamp;
+  } else {
+    var mom = moment(object.timestamp);
+    if (mom.isValid()) {
+      //Timestamp has a valid ISO 8601 format, convert it to the database format
+      ts = mom.format('DD MM YYYY, HH:mm:ss:SSS');
+    } else {
+      //Timestamp has no supported format, return an error
+      res.status(400).send("Timestamp has no valid format. Specify it in ISO 8601 or use this schema: DD MM YYYY, HH:mm:ss:SSS");
+    }
+  }
+  console.log('Update value ' + object.value + ' (TS_req='+object.timestamp+',TS_db='+ts+'): ' + _pipeline_id + " in " + _dataset_id);
 
   var value = new models.Value({
     _dataset: _dataset_id,
--- Frontend/app/scripts/services/userservice.js
+++ Frontend/app/scripts/services/userservice.js
@@ -3,14 +3,14 @@
 /**
  * @ngdoc service
  * @name pipelinrApp.UserService
- * @description
- * # UserService
- * Factory in the pipelinrApp.
- */
+* @description
+* # UserService
+* Factory in the pipelinrApp.
+*/
 angular.module('pipelinrApp')
   .factory('UserService', function ($resource) {
-    return $resource('http://localhost:1080' + '/users', {}, {  
-        query: {method: 'GET', isArray: true},
-        create: {method: 'POST'}
+    return $resource('http://localhost:1080' + '/users', {}, {
+      query: {method: 'GET', isArray: true},
+      create: {method: 'POST'}
     });
   });
